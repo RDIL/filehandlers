91e5467fdc605d0177ee305dd9cb765b5d090b4a
Reece Dunham <me@rdil.rocks>
2019-06-06 09:31:27 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='filehandlers',
+    version='1.0',
+    description='Package containing code to help in working with files',
+    packages=setuptools.find_packages(),
+    author='Cakebot Team',
+    author_email='cakebot@rdil.rocks',
+    url='https://github.com/cakebotpro/filehandlers'
+)
