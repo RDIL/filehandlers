757be5ebb518f872eb1354eb1d231903dc8cd6d9
Reece Dunham <me@rdil.rocks>
2019-07-23 18:50:39 -0700

    Fix: include long description

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 import setuptools
 
+with open("README.md", mode="r") as FILE_HANDLER:
+    LONG_DESCRIPTION = FILE_HANDLER.read()
+
 setuptools.setup(
     name='filehandlers',
     version='2.2',
-    description='Package containing code to help in working with files',
+    license="MIT",
+    description='Package containing code to help in working with files.',
     packages=setuptools.find_packages(),
     author='Reece Dunham',
     author_email='me@rdil.rocks',
@@ -12,5 +16,8 @@ setuptools.setup(
         'Documentation': 'https://filehandlers.readthedocs.io',
         'Source Code': 'https://github.com/RDIL/filehandlers',
         'Bug Tracker': 'https://github.com/RDIL/filehandlers/issues'
-    }
+    },
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    include_package_data=True
 )
