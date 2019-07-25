abf7b2f65fada6568d1ba994834fad56fa81c491
Reece Dunham <me@rdil.rocks>
2019-07-24 17:41:41 -0700

    publish thing

--- docs/conf.py
+++ docs/conf.py
@@ -48,6 +48,8 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_extra_path = ['robots.txt']
+
 # If true, copyright is shown in the HTML footer
 html_show_copyright = True
 
